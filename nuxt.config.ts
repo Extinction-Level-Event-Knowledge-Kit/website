f2e889e6b924378cbdec98d6b88ed546a6264cfc
Ron Appleton <ron.appleton@gmail.com>
2023-03-26 16:29:27 +0100

    chore: add configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    title: 'Extinction Level Event Knowledge Kit',
+    theme: {
+        dark: true,
+        colors: {
+            primary: '#ff0000',
+        }
+    },
     typescript: {
         strict: true,
+    },
+    buildModules: [
+        '@nuxt/typescript-build',
+        '@nuxtjs/composition-api/module',
+    ],
+    runtimeConfig: {
+        public: {
+            apiBase: process.env.API_BASE,
+            apiClientId: process.env.API_CLIENT_ID,
+            apiClientSecret: process.env.API_CLIENT_SECRET,
+        }
     }
 })
